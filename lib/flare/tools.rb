e0f6e71e043f3c5818252c52a677cc199f69c589
Kiyoshi Ikehara <kiyoshi.ikehara@gree.co.jp>
2011-11-24 12:01:09 +0900

    incremented the revision number. (0.2.1 -> 0.2.2)

--- lib/flare/tools.rb
+++ lib/flare/tools.rb
@@ -9,7 +9,7 @@ module Flare
   # flare-tools module.
   module Tools
     # the version number of flare-tools
-    VERSION = '0.2.1'
+    VERSION = '0.2.2'
     autoload :Common,      'flare/tools/common'
     autoload :Cluster,     'flare/tools/cluster'
     autoload :Stats,       'flare/tools/stats'
