da015b7ddfa5774c0b5bafbba7537894552f072c
Kiyoshi Ikehara <kiyoshi.ikehara@gree.co.jp>
2012-01-10 11:38:34 +0900

    added FlareClient class to partition_test.rb.

--- test/partition_test.rb
+++ test/partition_test.rb
@@ -9,6 +9,59 @@ require 'flare/tools/cli'
 require 'flare/test/cluster'
 require 'subcommands'
 
+class Result
+  def initialize(resultq)
+    @resultq = resultq
+  end
+
+  def get
+    @resultq.pop
+  end
+
+  def sync
+    @resultq.pop
+  end
+end
+
+class FlareClient
+  def initialize(node)
+    @queue = Queue.new
+    @resultq = Queue.new
+    @node = node
+    @th = Thread.new do
+      while item = @queue.pop
+        case item[0]
+        when :get
+          value = @node.get(item[1])
+          @resultq.push(value)
+        when :set
+          @node.set(item[1], item[2])
+          @resultq.push(nil)
+        when :quit 
+          @node.quit
+          break
+        end
+      end
+    end
+  end
+
+  def get(key)
+    @queue.push([:get, key.to_s])
+    Result.new(@queue)
+  end
+
+  def set(key, value)
+    @queue.push([:get, key.to_s, value.to_s])
+    Result.new(@queue)
+  end
+
+  def quit
+    @queue.push(:quit)
+    @th.join
+  end
+end
+
+
 class PartitionTest < Test::Unit::TestCase
   include Flare::Tools::Common
   include Subcommands
@@ -19,7 +72,9 @@ class PartitionTest < Test::Unit::TestCase
   def setup
     @flare_cluster = Flare::Test::Cluster.new('test')
     sleep 1 # XXX
-    @node_servers = ['node1', 'node2', 'node3'].map {|name| @flare_cluster.create_node(name)}
+    @node_servers = ['node1', 'node2', 'node3', 'node4', 'node5'].map {|name|
+      @flare_cluster.create_node(name)
+    }
     sleep 1 # XXX
     @flare_cluster.wait_for_ready
     @config = {
@@ -41,16 +96,12 @@ class PartitionTest < Test::Unit::TestCase
   def test_dynamic_partition_creation1
     @flare_cluster.prepare_master_and_slaves(@node_servers[0..1])
     @flare_cluster.prepare_data(@node_servers[0], "key", 1000)
-    queue = Queue.new
-    th = Thread.new do
-      while item = queue.pop
-        @nodes[0].set("hoge#{item}", "piyo")
-      end
-    end
-    args = @node_servers[2..3].map{|n| "#{n.hostname}:#{n.port}:1:1"} << "--force"
+    master = FlareClient.new(@nodes[0])
+    args = @node_servers[2...3].map{|n| "#{n.hostname}:#{n.port}:1:1"} << "--force"
     assert_equal(S_OK, master(*args))
     for i in 0..10
-      queue.push "#{i}"
+      r = master.set("k#{i}", "piyo")
+      r.sync
       sleep 1
       master_items = @nodes[0].stats["curr_items"].to_i
       slave_items = @nodes[1].stats["curr_items"].to_i
@@ -59,8 +110,29 @@ class PartitionTest < Test::Unit::TestCase
     end
     args = @node_servers[2..3].map{|n| "#{n.hostname}:#{n.port}"} << "--force"
     assert_equal(S_OK, activate(*args))
-    queue.push false
-    th.join
+    master.quit
+  end
+
+  def test_dynamic_partition_creation2
+    @flare_cluster.prepare_master_and_slaves(@node_servers[0..1])
+    @flare_cluster.prepare_data(@node_servers[0], "key", 1000)
+    master = FlareClient.new(@nodes[0])
+    args = @node_servers[2...3].map{|n| "#{n.hostname}:#{n.port}:1:1"} << "--force"
+    assert_equal(S_OK, master(*args))
+    args = @node_servers[3...4].map{|n| "#{n.hostname}:#{n.port}:0:1"} << "--force"
+    assert_equal(S_OK, slave(*args))
+    for i in 0..10
+      r = master.set("k#{i}", "piyo")
+      r.sync
+      sleep 1
+      master_items = @nodes[0].stats["curr_items"].to_i
+      slave_items = @nodes[1].stats["curr_items"].to_i
+      puts "master_items=#{master_items}, slave_items=#{slave_items}"
+      assert_equal(master_items, slave_items)
+    end
+    args = @node_servers[2..3].map{|n| "#{n.hostname}:#{n.port}"} << "--force"
+    assert_equal(S_OK, activate(*args))
+    master.quit
   end
 
 end
