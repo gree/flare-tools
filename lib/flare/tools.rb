74a952a93f619f8d7321c455f06528650df3a128
Kiyoshi Ikehara <kiyoshi.ikehara@gree.co.jp>
2012-01-12 19:12:18 +0900

    modified History.txt for version 0.2.6. incremented the revision number. (0.2.5 -> 0.2.6)

--- lib/flare/tools.rb
+++ lib/flare/tools.rb
@@ -9,7 +9,7 @@ module Flare
   # flare-tools module.
   module Tools
     # the version number of flare-tools
-    VERSION = '0.2.5'
+    VERSION = '0.2.6'
     TITLE = "Flare-tools version #{VERSION} Copyright (C) GREE, Inc. 2011-2012"
     autoload :Common,      'flare/tools/common'
     autoload :Cluster,     'flare/tools/cluster'
