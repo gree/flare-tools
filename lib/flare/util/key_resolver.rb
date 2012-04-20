06703da103f4fca98cc4e6bd6e72a674827996e5
Kiyoshi Ikehara <kiyoshi.ikehara@gree.co.jp>
2012-04-20 19:26:00 +0900

    fixed verify tests again.

--- lib/flare/util/key_resolver.rb
+++ lib/flare/util/key_resolver.rb
@@ -9,7 +9,7 @@ module Flare
           @hint = params[:hint] || 1
           @map = Array.new(@partition_size+1).map!{Array.new(@virtual, -1)}
           @next_calculate = 0
-          calculate 4096
+          calculate 1
         end
 
         def calculate psize
