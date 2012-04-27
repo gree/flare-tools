212b9a7e9eca49aa6e469733a46d7560d04a1a94
Kiyoshi Ikehara <kiyoshi.ikehara@gree.co.jp>
2012-04-27 12:30:48 +0900

    refactored dump_test.rb.

--- test/test/integration/dump_test.rb
+++ test/test/integration/dump_test.rb
@@ -24,26 +24,23 @@ class DumpTest < Test::Unit::TestCase
     File.delete("src.tch") if File.exist?("src.tch")
   end
 
-  def prepare cluster
+  def prepare cluster, partitions, ranges
     sleep 1 # XXX
     nodes = (1..6).map {|n| cluster.create_node("node#{n}")}
     sleep 1 # XXX
     cluster.wait_for_ready
-    cluster.prepare_master_and_slaves(nodes[0..1], 0)
-    cluster.prepare_master_and_slaves(nodes[2..3], 1)
-    cluster.prepare_master_and_slaves(nodes[4..5], 2)
+    partitions.each do |p|
+      cluster.prepare_master_and_slaves(nodes.values_at(*ranges[p]), p)
+    end
     nodes
   end
 
-  def dump_and_restore(range, expected, *prefixes, &block)
+  def dump_and_restore(range, prefixes, extra_dump_options, extra_restore_options, &check)
     # prepare
+    ranges = [[0..1], [2..3], [4..5]]
     src_cluster = Flare::Test::Cluster.new('src')
     puts "preparing src"
-    src_nodes = prepare src_cluster
-    puts "preparing dest"
-    dest_cluster = Flare::Test::Cluster.new('dest')
-    dest_nodes = prepare dest_cluster
-    
+    src_nodes = prepare(src_cluster, [0], ranges)
     puts "storing"
     range.each do |i|
       src_nodes[0].open do |n|
@@ -52,6 +49,10 @@ class DumpTest < Test::Unit::TestCase
         end
       end
     end
+    src_nodes = prepare(src_cluster, [1, 2], ranges)
+    puts "preparing dest"
+    dest_cluster = Flare::Test::Cluster.new('dest')
+    dest_nodes = prepare(dest_cluster, [0, 1, 2], ranges)
 
     puts "dumping"
     @config = {
@@ -59,7 +60,8 @@ class DumpTest < Test::Unit::TestCase
       :index_server_port => src_cluster.indexport,
       :timeout => 10
     }
-    args = %w(--all --format=tch --output=src.tch)
+    args = %w(--format=tch --output=src.tch)
+    args.concat extra_dump_options
     assert_equal(S_OK, dump(*args))
 
     @config = {
@@ -69,7 +71,7 @@ class DumpTest < Test::Unit::TestCase
     }
     n = dest_nodes[0]
     args = %w(--format=tch --input=src.tch)
-    block.call(args) unless block.nil?
+    args.concat extra_restore_options
     args << "#{n.hostname}:#{n.port}"
     assert_equal(S_OK, restore(*args))
 
@@ -79,7 +81,7 @@ class DumpTest < Test::Unit::TestCase
         r+n.stats["curr_items"].to_i
       end
     end
-    assert_equal(expected, restored)
+    check.call(restored)
 
     # destroy
     src_cluster.shutdown
@@ -87,14 +89,20 @@ class DumpTest < Test::Unit::TestCase
   end
 
   def test_dump_and_restore1
-    dump_and_restore (0...100), 100, "prefix", "xiferp", "pre" do |args|
-      args << '--include=^prefix'
+    dump_and_restore (0...100), ["prefix", "xiferp", "pre"], %w(--all), %w(--include=^prefix) do |restored|
+      assert_equal(100, restored)
     end
   end
 
   def test_dump_and_restore2
-    dump_and_restore (0...100), 100, "prefix", "xiferp", "pre" do |args|
-      args << '--prefix-include=prefix'
+    dump_and_restore (0...100), ["prefix", "xiferp", "pre"], %w(--all), %w(--prefix-include=prefix) do |restored|
+      assert_equal(100, restored)
+    end
+  end
+
+  def test_dump_and_restore3
+    dump_and_restore (0...100), ["prefix", "xiferp", "pre"], %w(--all --raw), %w(--prefix-include=prefix) do |restored|
+      assert_equal(100, restored)
     end
   end
 
