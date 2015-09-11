6b8f8d30dd5bb583a7fd300f035fdc0c5385cd83
Yuya YAGUCHI <yuya.yaguchi@gree.net>
2015-09-11 08:55:18 +0000

    Bump up version to 0.7.1

--- lib/flare/tools.rb
+++ lib/flare/tools.rb
@@ -9,8 +9,8 @@ module Flare
   # flare-tools module.
   module Tools
     # the version number of flare-tools
-    VERSION = '0.7.0'
-    TITLE = "Flare-tools version #{VERSION} Copyright (C) GREE, Inc. 2011-2014"
+    VERSION = '0.7.1'
+    TITLE = "Flare-tools version #{VERSION} Copyright (C) GREE, Inc. 2011-2015"
     autoload :Common,      'flare/tools/common'
     autoload :Cluster,     'flare/tools/cluster'
     autoload :Stats,       'flare/tools/stats'
